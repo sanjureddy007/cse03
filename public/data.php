6550a9d80daf1bfcd8a18434d9a12acbce7c95ab
Punith Srinivas <puniths181296@gmail.com>
2020-05-19 18:59:13 -0400

    commit

---
+++ public/data.php
@@ -0,0 +1,35 @@
+<?php
+
+function get_rating($name)
+{
+        /* Database INFO */
+	$servername = "localhost";
+	$username = "mudupus1";
+	$password = "u9nd56";
+	$dbname = "mudupus1_db";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+       }
+
+       $sql = "SELECT rating FROM movies WHERE title = '$name'";
+       $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+             // output data of each row
+             while($row = $result->fetch_assoc()) {
+                      $rating = $row["rating"];
+      }
+    } else {
+                     $rating = null;
+        }
+
+    $conn->close();
+
+    return $rating;
+}
+
+?>
